8ed5b18fa9ea6e03c0176b2c4afc630374956f1f
Chris McCluskey <christopher.i.mccluskey@gmail.com>
2017-02-23 19:00:24 +0000

    Platforms joining doors

--- Assets/LevelGenerator.cs
+++ Assets/LevelGenerator.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Text;
 using UnityEngine.SceneManagement;
+using System.Linq;
+using System;
 
 public class LevelGenerator : MonoBehaviour {
 
@@ -55,20 +57,21 @@ public class LevelGenerator : MonoBehaviour {
 		}
 	}
 
-	void PlacePlatforms () {
-		//var platform = GameObject.Find ("Platform36x01");
-		// starting platform
+	void PlacePlatforms (Vector3 start, Vector3 end) {
 
-		// end platform (with box)
+		// figure out how many platforms needed
+		var x_distance = end.x - start.x;
+		var y_distance = end.y - start.y;
 
-		var positions = generatePositions (30, 20);
-		foreach (var position in positions) {
-			var newPlatform = Instantiate(platform, position, Quaternion.identity);
+		var number_of_platforms = Math.Ceiling(x_distance / 6);
+
+		// space platforms evenlyish - constraints need to take into account diagonal distance
+		for (int i=1; i <= number_of_platforms; i++) {
+//			var x = UnityEngine.Random.Range (i+6, i + );
+			var y = UnityEngine.Random.Range (0.5f, 2f);
+			var newPlatform = Instantiate(platform, new Vector3(i*6, y, 0), Quaternion.identity);
 			newPlatform.transform.parent = transientParent;
 		}
-
-		var finalPlatform = Instantiate(platform, new Vector3(22,22,0), Quaternion.identity);
-		finalPlatform.transform.parent = transientParent;
 
 	}
 
@@ -76,8 +79,8 @@ public class LevelGenerator : MonoBehaviour {
 		var vectors =  new List<Vector3> ();
 
 		for (float i = 0; i < 5; i++) {
-			var x = Random.Range (i+2, i + 4);
-			var y = Random.Range (i, i + 2);
+			var x = UnityEngine.Random.Range (i+2, i + 4);
+			var y = UnityEngine.Random.Range (i, i + 2);
 			vectors.Add(new Vector3(x, y, 0) );
 		}
 
@@ -98,16 +101,31 @@ public class LevelGenerator : MonoBehaviour {
 
 			currentLevel = nextLevel;
 			var level = connections [currentLevel];
-			// do work to draw the level	
-			var levelEnd = GameObject.Find ("ExitPlatform");
-			foreach (var exit in level) {
-				var thing = Instantiate(levelEnd, new Vector3 ((exit*2) + 3f, 0.5f, 0), Quaternion.identity);
-				thing.transform.parent = transientParent;
-				thing.SendMessage("SetLevel", exit);
-			}
 
-			//PlacePlatforms ();
+			// do work to draw the level
+			MakeLevel (level);
+
 		}
+	}
+
+	void MakeLevel(HashSet<int> exits) {
+		var levelEnd = GameObject.Find ("ExitPlatform");
+
+		var min = exits.Min	();
+		var max = exits.Max ();
+			
+		if (min != max) {
+			var back = Instantiate (levelEnd, new Vector3 (0f, 0.5f, 0), Quaternion.identity);
+			back.transform.parent = transientParent;
+			back.SendMessage ("SetLevel", min);
+		}
+
+
+		var forwards = Instantiate(levelEnd, new Vector3 (42f, 0.5f, 0), Quaternion.identity);
+		forwards.transform.parent = transientParent;
+		forwards.SendMessage("SetLevel", max);
+
+		PlacePlatforms (new Vector3 (0f, 0.5f, 0), new Vector3 (42f, 0.5f, 0));
 	}
 
 
@@ -120,11 +138,11 @@ public class LevelGenerator : MonoBehaviour {
 	List<HashSet<int>> AddNewConnection(List<HashSet<int>> connections){
 		var max = connections.Count - 1;
 
-		var a = (int)Random.Range (0, max);
-		var b = (int)Random.Range (0, max);
+		var a = (int)UnityEngine.Random.Range (0, max);
+		var b = (int)UnityEngine.Random.Range (0, max);
 
 		while (a == b) {
-			b = (int)Random.Range (0, max);
+			b = (int)UnityEngine.Random.Range (0, max);
 		}
 
 		connections [a].Add (b);
